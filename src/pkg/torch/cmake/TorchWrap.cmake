e0684a19473eeeb5ca96288c29982b174b4792ef
Boris Fomitchev <bfomitchev@nvidia.com>
2016-01-20 16:31:38 -0800

    Fixing torchcwrap

--- src/pkg/torch/cmake/TorchWrap.cmake
+++ src/pkg/torch/cmake/TorchWrap.cmake
@@ -2,10 +2,12 @@ MACRO(ADD_TORCH_WRAP target luafile)
   INCLUDE_DIRECTORIES("${CMAKE_CURRENT_BINARY_DIR}")
   GET_FILENAME_COMPONENT(_file_ "${luafile}" NAME_WE)
   SET(cfile "${_file_}.c")
+  SET(wrapdir "${CMAKE_CURRENT_SOURCE_DIR}/../cwrap")
+  SET($ENV{LUA_PATH} "./?.lua;${CMAKE_CURRENT_SOURCE_DIR}/?.lua;${wrapdir}/?.lua;${LUA_PATH};")
+
   ADD_CUSTOM_COMMAND(
     OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${cfile}"
-    COMMAND ${LUA} ARGS "${CMAKE_CURRENT_SOURCE_DIR}/${luafile}" "${CMAKE_CURRENT_BINARY_DIR}/${cfile}"
-    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-    DEPENDS "${luafile}")
+    COMMAND luajit ARGS "-e \"dofile('${wrapdir}/init.lua')\" " "${CMAKE_CURRENT_SOURCE_DIR}/${luafile}" "${CMAKE_CURRENT_BINARY_DIR}/${cfile}"
+    DEPENDS "${luafile}" "${wrapdir}/init.lua" "${wrapdir}/types.lua")
   ADD_CUSTOM_TARGET(${target} DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/${cfile}")
 ENDMACRO(ADD_TORCH_WRAP)
